e43e2495102eb6278243e0dde2f8a7a256aa5ce7
Jose Ojeda <joche.ojeda@bitframeworks.com>
2025-01-29 21:49:39 +0300

    null protection

--- src/AppPerformanceTracker.Contracts/FileMethodPerformanceTracker.cs
+++ src/AppPerformanceTracker.Contracts/FileMethodPerformanceTracker.cs
@@ -5,15 +5,15 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace AppPerformanceTracker.Contracts
 {
-    public class FileMethodPerformanceTracker : MethodPerformanceTracker
+    public class FileMethodPerformanceTracker : MethodPerformanceTracker, IDisposable
     {
         private static FileMethodPerformanceTracker _instance;
         private static readonly object _instanceLock = new object();
         private readonly string _logFilePath;
         private readonly StreamWriter _streamWriter;
+        private bool _disposed = false;
 
         private FileMethodPerformanceTracker(string logFilePath)
         {
@@ -51,11 +51,11 @@ namespace AppPerformanceTracker.Contracts
             }
         }
 
-        public override void RecordExecution(string AppId,string SessionId, MethodBase method, object[] parameters, TimeSpan duration, DateTime dateTime)
+        public override void RecordExecution(string appId, string sessionId, MethodBase method, object[] parameters, TimeSpan duration, DateTime dateTime)
         {
-            base.RecordExecution(AppId,SessionId, method, parameters, duration, dateTime);
+            base.RecordExecution(appId, sessionId, method, parameters, duration, dateTime);
 
-            MethodExecutionDto execution = MethodExecutionDto.Create(AppId, SessionId, method, parameters, duration, dateTime);
+            MethodExecutionDto execution = MethodExecutionDto.Create(appId, sessionId, method, parameters, duration, dateTime);
 
             var logEntry = JsonConvert.SerializeObject(execution);
             var encodedLogEntry = Convert.ToBase64String(Encoding.UTF8.GetBytes(logEntry));
@@ -68,13 +68,33 @@ namespace AppPerformanceTracker.Contracts
 
         private async Task WriteLogEntryAsync(string logEntry)
         {
+            if (_disposed) throw new ObjectDisposedException(nameof(FileMethodPerformanceTracker));
+
             await _streamWriter.WriteLineAsync(logEntry);
             await _streamWriter.FlushAsync();
         }
 
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _streamWriter?.Dispose();
+                }
+                _disposed = true;
+            }
+        }
+
         ~FileMethodPerformanceTracker()
         {
-            _streamWriter?.Dispose();
+            Dispose(false);
         }
     }
 }
